3afc989a1140d69c35ddb731d93553060111144c
saminakhan999 <Samina.khan9999@gmail.com>
2022-02-01 15:11:02 +0000

    fixeddd

--- server/index.js
+++ server/index.js
@@ -1,13 +1,5 @@
 
-const app = require('./app.js');
-const port = 3000;
-
-app.listen(port, () => { console.log(`Express now departing from port ${port}`)})
+const server = require("./app")
 
 
-
-
-/*const server = require("./app")
-
-
-server.listen(process.env.PORT || 3000, () => console.log("Express now departing from port")) */
+server.listen(process.env.PORT || 3000, () => console.log("Express now departing from port")) 
